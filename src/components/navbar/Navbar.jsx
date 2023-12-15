39d96afbe64e1c221f659b03fdf15a8f63f779b5
hoadesign09 <hoadesign.09@gmail.com>
2023-12-15 15:36:24 +0700

    Client upload

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,17 +1,30 @@
-import "./navbar.css"
+import { useContext } from "react";
+import "./navbar.css";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
 
 const Navbar = () => {
+  const { user } = useContext(AuthContext);
+
   return (
     <div className="navbar">
       <div className="navContainer">
-        <span className="logo">lamabooking</span>
-        <div className="navItems">
-          <button className="navButton">Register</button>
-          <button className="navButton">Login</button>
-        </div>
+        <Link to="/">
+          <span className="logo">Booking App</span>
+        </Link>
+        {user ? (
+          user.username
+        ) : (
+          <div className="navItems">
+            <Link to="/login">
+              <button className="navButton">Login</button>
+            </Link>
+            <button className="navButton">Register</button>
+          </div>
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
